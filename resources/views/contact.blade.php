02dff6bab2055f297b8a93386e7bf19eb24e10c4
Dayannis <dayannis.y@gmail.com>
2017-04-19 12:39:24 -0400

    Content

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,68 @@
+	<div class="container content-lg">
+		<div class="title-v1">
+			<h2>Contáctenos</h2>
+			<p>¡Gracias por confiar en nosotros!. Próximamente atenderemos su requerimiento, por favor esté atento de su buzón de entrada o carpeta spam.</p>
+			</div>
+
+			<div class="row contacts-in">
+				<div class="col-md-6 md-margin-bottom-40">
+					<ul class="list-unstyled">
+						<li><i class="fa fa-home"></i> Urbanización Las Cayenas, Manzana 7, Calle 4, Nº 68. <br>Maturín, Estado Monagas. Venezuela</li>
+						<li><i class="fa fa-phone"></i> (0291) 3159504 – (0414) 8584909 </li>
+						<li><i class="fa fa-envelope"></i> gestiadmundifacil@gmail.com </li>
+						<li><i class="fa fa-globe"></i> Gestiadmundi Siglo XXI J–404343539 </li>
+					</ul>
+				</div>
+
+				<div class="col-md-6">
+					<form action="assets/php/sky-forms-pro/demo-contacts-process.php" method="post" id="sky-form3" class="sky-form contact-style">
+						<fieldset>
+							<label>Nombre</label>
+							<div class="row">
+								<div class="col-md-7 margin-bottom-20 col-md-offset-0">
+									<div>
+										<input type="text" name="name" id="name" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<label>Email <span class="color-red">*</span></label>
+							<div class="row">
+								<div class="col-md-7 margin-bottom-20 col-md-offset-0">
+									<div>
+										<input type="text" name="email" id="email" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<label>Mensaje</label>
+							<div class="row">
+								<div class="col-md-11 margin-bottom-20 col-md-offset-0">
+									<div>
+										<textarea rows="8" name="message" id="message" class="form-control"></textarea>
+									</div>
+								</div>
+							</div>
+
+							<p><button type="submit" class="btn-u btn-brd btn-brd-hover btn-u-dark">Enviar</button></p>
+						</fieldset>
+
+						<div class="message">
+							<i class="rounded-x fa fa-check"></i>
+							<p>¡Tu mensaje fue enviado!</p>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<div class="copyright-section">
+			<p> Gestiadmundi Siglo XXI   &copy; Copyrigth 2017<br>J–404343539<br>Desarrollado por <a target="_blank" href="https://www.linkedin.com/in/dayannis-alguaca/">Dayannis Alguaca</a></p>
+			<ul class="social-icons">
+				<li><a href="#" data-original-title="Facebook" class="social_facebook rounded-x"></a></li>
+				<li><a href="#" data-original-title="Twitter" class="social_twitter rounded-x"></a></li>
+				<li><a href="#" data-original-title="Goole Plus" class="social_googleplus rounded-x"></a></li>
+				<li><a href="#" data-original-title="Linkedin" class="social_linkedin rounded-x"></a></li>
+			</ul>
+			<span class="page-scroll"><a href="#intro"><i class="fa fa-angle-double-up back-to-top"></i></a></span>
+		</div>
---
+++ resources/views/services.blade.php
@@ -0,0 +1,144 @@
+<div class="container content-lg">
+	<div class="title-v1">
+		<h2>Servicios</h2>
+		<p>Nuestra organización le Ofrece los siguientes servicios de Consultoría Gerencial.</p>
+	</div>
+
+		<div class="row service-box-v1">
+			<div class="col-md-4 col-sm-6">
+				<div class="service-block service-block-default">
+					<i class="icon-custom icon-lg icon-bg-u rounded-x fa fa-lightbulb-o"></i>
+					<h2 class="heading-md">Consultoría Gerencial Administrativa</h2>
+					<p>Incluye los servicios de:</p>
+					<ul class="list-unstyled">
+						<li>Suministro de personal. </li>
+						<li>Asesorías en la toma de Decisiones y Gestión para la Proyección Empresarial.</li>
+						<li>Ubicación de Contratos en las diferentes aéreas del Mercado Laboral de acuerdo con el funcionamiento  de las especialidades de su Negocio y/o Empresa.  </li>
+						<li>Asesorías y Estrategias en el Área de Crecimiento Financiero y de Negocios Nacionales e Internacionales. </li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-6">
+				<div class="service-block service-block-default">
+					<i class="icon-custom icon-lg icon-bg-u rounded-x icon-line icon-present"></i>
+					<h2 class="heading-sm">Consultoría Comercial</h2>
+					<p>Incluye los servicios de:</p>
+					<ul class="list-unstyled">
+						<li>Asesoría y estrategias en  el Área Comercial y de Proyección con miras Empresariales para su Negocio y/o Empresa.</li>
+						<li>Ubicación del Producto de su Negocio y/o Empresa en el Mercado Productivo y Laboral a nivel Nacional e Internacional según sea su Requerimiento.</li>
+						<li>Valoración de Negocios y/o Empresas  con fines de Negociación e inversión involucradas con usted.</li>
+						<li>Evaluación Financiera de Proyectos o servicios para los Clientes y Proveedores involucrados con su Negocio y/o Empresa</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-12">
+				<div class="service-block service-block-default">
+					<i class="icon-custom icon-lg icon-bg-u rounded-x icon-line icon-rocket"></i>
+					<h2 class="heading-sm">Consultoría Contable</h2>
+					<p>Incluye los servicios de:</p>
+					<ul class="list-unstyled">
+						<li>Servicios de Contabilidad  tributaria y Asesoría en materia de toma de decisiones para el Cumplimiento Normativo de los Estados Financieros, los reportes tributarios y la seguridad social de su Negocio y/o Empresa.</li>
+						<li>Servicios Especiales y Facilidad a los Contribuyentes especiales.</li>
+						<li>Asesoría para el montaje del sistema de información contable y financiero y acompañamiento en el proceso Contable de su Negocio y/o Empresa.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="row service-box-v1">
+			<div class="col-md-6 col-sm-6">
+				<div class="service-block service-block-default">
+					<i class="icon-custom icon-lg icon-bg-u rounded-x fa fa-lightbulb-o"></i>
+					<h2 class="heading-md">Consultoría Contable y Financiero</h2>
+					<p>Adicional al Servicio contable, incluye:</p>
+					<ul class="list-unstyled">
+						<li>El análisis financiero histórico y proyectado a partir de un modelo implementado y ajustado en cada Organización.</li>
+						<li>Análisis histórico y proyectado de la información financiera para la toma de decisiones de inversión y financiación.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<div class="service-block service-block-default">
+					<i class="icon-custom icon-lg icon-bg-u rounded-x icon-line icon-present"></i>
+					<h2 class="heading-sm">Consultoría Administrativa:</h2>
+					<p>Incluye los servicios de:</p>
+					<ul class="list-unstyled">
+						<li>Asesoría y acompañamiento en procesos Administrativos en cuanto a la Gestión del Recurso Humano.</li>
+						<li>Estrategias Empresariales, Procesos Operativos, Legales, Sociales y Ambientales.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row service-box-v1">
+			<div class="col-md-12 col-sm-12">
+				<div class="service-block service-block-default">
+					<i class="icon-custom icon-lg icon-bg-u rounded-x icon-line icon-rocket"></i>
+					<h2 class="heading-sm">Consultoría de Servicios Especiales:</h2>
+					<p>Incluye los servicios de:</p>
+					<ul class="list-unstyled">
+						<li>Consultoría de Servicios Financieros Especiales, Administrativos, Operativos, Legales, Sociales y de otras áreas estratégicas de su Negocio y/o Empresa.</li>
+						<li>Asesoría , Elaboración y Gestión de Proyectos.</li>
+						<li>Servicios de  Comercialización y Cobranzas.</li>
+						<li>Asesoría y Gestión Para tener al día su Negocio y/o Empresa en  los diferentes Organismos del Estado, como podemos mencionar: <br><small>
+						INCE Tributos(Inscripción y Solvencia).<br>
+						INCE: PNA (Asistencia, Asesoría y Gestión).<br>
+						S.S.O (Inscripción y solvencia).<br>
+						ALCALDIA (Inscripción y Solvencia).<br>
+						GOBERNACION (Inscripción y solvencias).<br>
+						PDVSA: RNC, SNC, (Inscripción y Solvencia).<br>
+						RUPDAE. (Inscripción).<br>
+						PDVSA:  Elaboración de Licitaciones, Estimaciones de Costos y A.P.U.<br>
+						SENIAT (Inscripción y Solvencia).<br>
+						ADUANA ( Asesoría, Nacionalización, Senncamer, Importación y Exportación)
+						MINISTERIO DE CIENCIA Y TECNOLOGIA (Inscripción y Solvencia).<br>
+						INTTT Instituto Nacional de Transito y Transporte Terrestre. (Registros, Jornadas, Permisos, Licencias entre Otros.<br>
+						SEGUROS: HCM, AUTOMOVIL, VIDA FUNERARIO, RESPONSABILIDAD CIVIL DE VEHICULOS, RESPONSAVILIDAD CIVIL GENERAL, PATRONAL, FIANZAS DE FIEL CUMPLIMIENTO, FIANZAS DE ANTICIPO, FIANZAS LABORAL, FIANZAS DE GARANTIAS.<br>
+						Ofrecimiento de Paquete de Medicina Administrada para Empresas.<br>
+  						Gestiones Inmobiliarias.</small></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!--
+	<ul class="list-unstyled row portfolio-box-v1">
+		<li class="col-sm-4">
+			<img class="img-responsive" src="assets/img/mockup/img1.jpg" alt="">
+			<div class="portfolio-box-v1-in">
+				<h3>Collective Package</h3>
+				<p>Web Design, Mock-up</p>
+				<a class="btn-u btn-u-sm btn-brd btn-brd-hover btn-u-light" href="#">Read More</a>
+			</div>
+		</li>
+		<li class="col-sm-4">
+			<img class="img-responsive" src="assets/img/mockup/img2.jpg" alt="">
+			<div class="portfolio-box-v1-in">
+				<h3>Ahola Company</h3>
+				<p>Brand Design, UI</p>
+				<a class="btn-u btn-u-sm btn-brd btn-brd-hover btn-u-light" href="#">Read More</a>
+			</div>
+		</li>
+		<li class="col-sm-4">
+			<img class="img-responsive" src="assets/img/mockup/img4.jpg" alt="">
+			<div class="portfolio-box-v1-in">
+				<h3>Allan Project</h3>
+				<p>Web Development, HTML5</p>
+				<a class="btn-u btn-u-sm btn-brd btn-brd-hover btn-u-light" href="#">Read More</a>
+			</div>
+		</li>
+	</ul>
+
+	<div class="call-action-v1">
+		<div class="container">
+			<div class="call-action-v1-box">
+				<div class="call-action-v1-in">
+					<p>Unify creative technology company providing key digital services and focused on helping our clients to build a successful business on web and mobile.</p>
+				</div>
+				<div class="call-action-v1-in inner-btn page-scroll">
+					<a href="#portfolio" class="btn-u btn-brd btn-brd-hover btn-u-dark btn-u-block">View Our Portfolio</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	-->
